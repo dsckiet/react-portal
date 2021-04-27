04b0f1c6539047b5b1986af8a593eb9adfc02a24
Aniket Padmansh <pudmaansh@gmail.com>
2021-04-27 13:14:47 +0530

    --fix : drawers responsive

--- src/components/Team/UserOptions.js
+++ src/components/Team/UserOptions.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Drawer, Row, Col } from "antd";
 import AddMember from "./AddMember";
 import { getRole } from "./../../utils/services";
@@ -7,10 +7,17 @@ import { getRole } from "./../../utils/services";
 const UserOptions = props => {
 	const [isDrawerVisible, setIsDrawerVisible] = useState(false);
 	const [userData] = useState(getRole());
+	const [isMobile, setIsMobile] = useState(false);
 	const handleUserAdd = () => {
 		setIsDrawerVisible(false);
 		props.onAddMember();
 	};
+
+	useEffect(() => {
+		if (window.innerWidth <= 568) {
+			setIsMobile(true);
+		}
+	}, []);
 
 	return (
 		<>
@@ -30,7 +37,7 @@ const UserOptions = props => {
 					title="Add New Member"
 					placement="right"
 					closable={true}
-					width="40%"
+					width={isMobile ? "80%" : "40%"}
 					destroyOnClose={true}
 					onClose={() => setIsDrawerVisible(false)}
 					visible={isDrawerVisible}
