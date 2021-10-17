56c942638dd0d457214eca44390f6c9050964213
Aniket Padmansh <pudmaansh@gmail.com>
2021-10-17 17:16:13 +0530

    --open L create event route

--- src/components/Layout/PrivateRoute.js
+++ src/components/Layout/PrivateRoute.js
@@ -1,21 +1,20 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "./../../contexts/userContext";
 import { Route, Redirect } from "react-router-dom";
-import { getRole } from "./../../utils/services";
+//import { getRole } from "./../../utils/services";
 
 const PrivateRoute = ({ component: Component, data, ...rest }) => {
 	const Data = useContext(AuthContext);
-	const [userData] = useState(getRole());
+	//const [userData] = useState(getRole());
 	return (
 		<Route
 			{...rest}
 			render={props =>
 				Data.token !== "" ? (
-					rest.path === "/groups" && userData.role === "member" ? (
-						<Redirect to="/login" />
-					) : (
-						<Component {...props} />
-					)
+					// rest.path === "/groups" && userData.role === "member" ? (
+					// 	<Redirect to="/login" />
+					// ) : (
+					<Component {...props} />
 				) : (
 					<Redirect to="/login" />
 				)
--- src/utils/_routes.js
+++ src/utils/_routes.js
@@ -1,18 +1,18 @@
 import EventsList from "../components/Events/EventsList";
 import Dashboard from "../components/Layout/Dashboard";
 import TeamList from "../components/Team/TeamList";
-import ParticipantsList from "../components/Participants/ParticipantsList";
+//import ParticipantsList from "../components/Participants/ParticipantsList";
 //import AddCertificate from "./../components/Certificates/AddCertificate";
 import Profile from "../components/Profile/Profile";
 import {
 	AiOutlineAppstore,
 	AiOutlineSmile,
-	AiOutlineTeam,
-	AiOutlineUsergroupAdd
+	AiOutlineTeam
+	//AiOutlineUsergroupAdd
 } from "react-icons/ai";
 import { MdEvent } from "react-icons/md";
-import { RiContactsLine } from "react-icons/ri";
-import ManageGroups from "../components/Groups/ManageGroups";
+//import { RiContactsLine } from "react-icons/ri";
+//import ManageGroups from "../components/Groups/ManageGroups";
 
 let routes = [
 	{
@@ -35,16 +35,16 @@ let routes = [
 		color: "#DB4437",
 		icon: MdEvent
 	},
-	{
-		path: "/participants",
-		exact: true,
-		component: ParticipantsList,
-		name: "Participants",
-		key: "participants",
-		icon: RiContactsLine,
-		description: "List of all the participants",
-		color: "#4285F4"
-	},
+	// {
+	// 	path: "/participants",
+	// 	exact: true,
+	// 	component: ParticipantsList,
+	// 	name: "Participants",
+	// 	key: "participants",
+	// 	icon: RiContactsLine,
+	// 	description: "List of all the participants",
+	// 	color: "#4285F4"
+	// },
 	{
 		path: "/team",
 		exact: true,
@@ -65,16 +65,16 @@ let routes = [
 	// 	description: "Add certificates for Events",
 	// 	color: "#F4B400"
 	// },
-	{
-		path: "/groups",
-		exact: true,
-		component: ManageGroups,
-		name: "Groups",
-		key: "groups",
-		icon: AiOutlineUsergroupAdd,
-		description: "Manage Groups",
-		color: "#DB4437"
-	},
+	// {
+	// 	path: "/groups",
+	// 	exact: true,
+	// 	component: ManageGroups,
+	// 	name: "Groups",
+	// 	key: "groups",
+	// 	icon: AiOutlineUsergroupAdd,
+	// 	description: "Manage Groups",
+	// 	color: "#DB4437"
+	// },
 	{
 		path: "/profile",
 		exact: true,
